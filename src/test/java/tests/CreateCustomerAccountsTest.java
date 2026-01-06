2931d431c89da0d1d6dd217aacc37d7c6f6cff5e
mariSuper <mmariamdan8@gmail.com>
2026-01-06 17:22:07 +0200

    Colectii

--- src/test/java/tests/CreateCustomerAccountsTest.java
+++ src/test/java/tests/CreateCustomerAccountsTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
--- src/test/java/tests/CreateCustomerTest.java
+++ src/test/java/tests/CreateCustomerTest.java
@@ -5,26 +5,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
+import java.util.List;
 
 public class CreateCustomerTest {
     public WebDriver driver;
 
     @Test
-
     public void automationTest() {
         driver = new ChromeDriver();
         driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        //button[@ng-click='addCust()']
-
-        WebElement bankManagerElement = driver.findElement(By.xpath("//button[@ng-click='manager()']"));
+        //declaram elementul meniu Bank Manager Login
+        WebElement bankManagerElement = driver.findElement(By.xpath("//button[text()='Bank Manager Login']"));
         bankManagerElement.click();
 
+        //declaram si submeniul 'Add Customer'
         WebElement addCustomerElement = driver.findElement(By.xpath("//button[@ng-click='addCust()']"));
         addCustomerElement.click();
 
@@ -43,17 +43,18 @@ public class CreateCustomerTest {
         WebElement submitCustomerElement = driver.findElement(By.xpath("//button[@type='submit']"));
         submitCustomerElement.click();
 
-        Alert customerAlert = driver.switchTo().alert();
+         Alert customerAlert = driver.switchTo().alert();
         String customerAlertText = customerAlert.getText();
         System.out.println(customerAlertText);
         customerAlert.accept();
 
+        // Open Acount button
         WebElement openAccountElement = driver.findElement(By.xpath("//button[@ng-click='openAccount()']"));
         openAccountElement.click();
 
         WebElement customerName = driver.findElement(By.id("userSelect"));
         Select customerSelect = new Select(customerName);
-        String fullNameValue = firstNameValue+ " " + lastNameValue;
+        String fullNameValue = firstNameValue + " " + lastNameValue;
         customerSelect.selectByVisibleText(fullNameValue);
 
         WebElement currency = driver.findElement(By.id("currency"));
@@ -61,19 +62,34 @@ public class CreateCustomerTest {
         String currencyValue = "Dollar";
         currencySelect.selectByVisibleText(currencyValue);
 
-        WebElement processButton = driver.findElement(By.xpath("//button[@type='submit']"));
-        processButton.click();
+        WebElement submitButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        submitButton.click();
 
+        // get text from the Alert
         Alert accountAlert = driver.switchTo().alert();
         String accountAlertText = accountAlert.getText();
         System.out.println(accountAlertText);
+        String [] accountsArray = accountAlertText.split(":");
+        String accountNumber = accountsArray[1];
+        System.out.println(accountsArray[1]);
         accountAlert.accept();
 
+        //Customers button
         WebElement customersButton = driver.findElement(By.xpath("//button[@ng-click='showCust()']"));
         customersButton.click();
 
+        //Adaugam un wait fortat care sa ne garanteze ca cele doua componente comunica intre ele
         WebElement searchCustomerElement = driver.findElement(By.xpath("//input[@placeholder='Search Customer']"));
-        searchCustomerElement.sendKeys(fullNameValue);
-    }
+        searchCustomerElement.sendKeys(firstNameValue);
 
+        List<WebElement> tableRows = driver.findElements(By.xpath("//tbody/tr"));
+        String customerTableRow = tableRows.get(0).getText();
+        Assert.assertTrue(customerTableRow.contains(firstNameValue));
+        Assert.assertTrue(customerTableRow.contains(lastNameValue));
+        Assert.assertTrue(customerTableRow.contains(postCodeValue));
+        Assert.assertTrue(customerTableRow.contains(accountNumber));
+
+        WebElement deletecustomerElement = driver.findElement(By.xpath("//button[@ng-click='deleteCust(cust)']"));
+        deletecustomerElement.click();
+    }
 }

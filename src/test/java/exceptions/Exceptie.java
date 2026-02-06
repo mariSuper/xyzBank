38d8c7c71154d6cb122daaf01b30bea5c0142c40
mariSuper <mmariamdan8@gmail.com>
2026-02-06 17:16:50 +0200

    Disable demo exception test from CI

--- src/test/java/exceptions/Exceptie.java
+++ src/test/java/exceptions/Exceptie.java
@@ -1,16 +1,15 @@
-package exceptions;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
-public class Exceptie {
+//package exceptions;
+//
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//
+//public class Exceptie {
     //Exceptie = o eroare ce apare in momentul in care programul ruleaza
     //exceptiile trebuie tratate deoarece contin informatii din interior care pot sa fie exploatate
     //ca sa tratam o exceptie se foloseste structura try & catch / finally
@@ -23,43 +22,43 @@ public class Exceptie {
     //throw => cuvant pe care il folosim cand vrem sa facem trigger la o exceptie
     //throws => cuvant pe care il folosim ca sa adaugam exceptia la semnatura metodei (VECI!!)
 
-    @Test
-    public void testMethod() {
-//        checkedExample();
-//        uncheckedExample();
-        throwExample();
-    }
+//    @Test
+//    public void testMethod() {
+////        checkedExample();
+////        uncheckedExample();
+//        throwExample();
+//    }
 
     //checked
-    public void checkedExample() {
-        String path = "src/CustomerData.json";
-        File file = new File(path);
-        try {
-            FileInputStream fileContent = new FileInputStream(file);
-        } catch (FileNotFoundException e) {
-        }
-    }
+//    public void checkedExample() {
+//        String path = "src/CustomerData.json";
+//        File file = new File(path);
+//        try {
+//            FileInputStream fileContent = new FileInputStream(file);
+//        } catch (FileNotFoundException e) {
+//        }
+//    }
 
     //unchecked
-    public void uncheckedExample() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://google.com");
-
-        //vreau sa gasesc un element
-        WebElement alex = driver.findElement(By.xpath("Alex Dorha"));
-        alex.click();
-    }
+//    public void uncheckedExample() {
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://google.com");
+//
+//        //vreau sa gasesc un element
+//        WebElement alex = driver.findElement(By.xpath("Alex Dorha"));
+//        alex.click();
+//    }
 
     //keyword specific: throw
-    public void throwExample() {
+//    public void throwExample() {
 //        String path = "src/CustomerData.json";
 //        File file = new File(path);
 //        try {
 //            FileInputStream fileContent = new FileInputStream(file);
 //        } catch (FileNotFoundException e) {
 //            throw new RuntimeException("Nu accept aceasta problema");
-        }
-    }
+//        }
+//    }
         //keyword specific: throws
 //        public void throwsExample () {
 //            String path = "src/CustomerData.json";
